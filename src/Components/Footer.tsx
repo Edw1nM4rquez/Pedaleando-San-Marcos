78586eb4f7c09455b7d32889e567fdc57e834dce
Edw1nM4rquez <edwi1999@hotmail.com>
2025-04-06 15:03:00 -0500

    feat: Adding logo page marquez.

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,15 +1,22 @@
 import { Footer, FooterCopyright } from "flowbite-react";
-import './Footer.scss'
+import "./Footer.scss";
+import casaMarquez from "./../assets/image/LA CASA MARQUEZ.png";
 function FooterComponent() {
   return (
     <Footer container className="rounded-none content-center footer">
-      <FooterCopyright href="#" by="By Desing - La Casa Marquez" year={2025} className="text-center"/>
-      {/* <FooterLinkGroup>
-        <FooterLink href="#">About</FooterLink>
+      <FooterCopyright
+        href="#"
+        by="By Desing - La Casa Marquez"
+        year={2025}
+        className="text-center"
+      />
+      <div className="absolute right-0 mr-2">
+        <img src={casaMarquez} alt="" className="h-16" />
+        {/* <FooterLink href="#">About</FooterLink>
         <FooterLink href="#">Privacy Policy</FooterLink>
         <FooterLink href="#">Licensing</FooterLink>
-        <FooterLink href="#">Contact</FooterLink>
-      </FooterLinkGroup> */}
+        <FooterLink href="#">Contact</FooterLink> */}
+      </div>
     </Footer>
   );
 }
